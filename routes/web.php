b793f628f99b6a009cc06464e8918e6b2a05f45f
Caua <caua.lopes@salvador.ba.gov.br>
2026-07-28 15:58:10 -0300

    chore: setup initial project structure

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\LivroControler;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('livros', LivroControler::class);
